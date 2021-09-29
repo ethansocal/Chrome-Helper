f599965deba071c412d0ab1b43af42b2bb31aab9
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2021-09-29 16:19:16 -0700

    Initial Commit
    Initial commit

---
+++ hack.js
@@ -0,0 +1,15 @@
+document.onkeydown = function(ev) {
+    if (ev.code === "Digit1") {
+        document.getElementsByClassName("fsDskO")[0].click();
+    }
+    else if (ev.code === "Digit2") {
+        document.getElementsByClassName("gBuyfW")[0].click();
+    }
+    else if (ev.code === "Digit3") {
+        document.getElementsByClassName("eepDQH")[0].click();
+    }
+    else if (ev.code === "Digit4") {
+        document.getElementsByClassName("dBaSvz")[0].click();
+    }
+    console.log(ev.code + " was pressed")
+};
