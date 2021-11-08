cc2de186c5d009a91ac8f53333c5d21c390aa541
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2021-11-08 12:26:00 -0800

    Add solve button

--- src/setgame/setgame.js
+++ src/setgame/setgame.js
@@ -4,3 +4,8 @@ s.onload = function() {
     this.remove();
 };
 (document.head || document.documentElement).appendChild(s);
+
+var button = document.createElement('a');
+button.text = "Solve";
+button.href = "javascript:for (set_num in board.theSets) {for (card_id in board.theSets[set_num]) {board.cardClicked(board.theSets[set_num][card_id]);}}"
+document.getElementsByClassName('yesterdays-solution')[0].appendChild(button)
